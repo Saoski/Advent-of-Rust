0872dfcbb9fae57e8caee546e333c90c6085dade
saoski <saoski88@gmail.com>
2024-12-08 16:50:11 -0500

    completed part 1

--- src/bin/3.rs
+++ src/bin/3.rs
@@ -1,6 +1,24 @@
-fn part_1() {}
+use regex::Regex;
+use std::fs::read_to_string;
 
-fn part_2() {}
+fn read_file(path: &str) -> String {
+    read_to_string(path).expect("Failed to read file")
+}
+
+fn part_1() {
+    let binding = read_file("inputs\\3");
+    let hay: &str = binding.as_str();
+    let re: Regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let mut sum: i32 = 0;
+    for (_, [num1, num2]) in re.captures_iter(hay).map(|c| c.extract()) {
+        sum += num1.parse::<i32>().unwrap() * num2.parse::<i32>().unwrap();
+    }
+    println!("{}", sum);
+}
+
+fn part_2() {
+    let hay = read_file("inputs\\3").as_str();
+}
 
 fn main() {
     part_1();
