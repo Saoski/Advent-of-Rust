9e0eafb140c77851464daac13284175aa1152710
saoski <saoski88@gmail.com>
2024-12-08 22:47:08 -0500

    debugging part 1

--- src/bin/4.rs
+++ src/bin/4.rs
@@ -1,4 +1,114 @@
-fn part_1() {}
+use core::array::from_fn;
+use std::fs::read_to_string;
+
+const WORD: &str = "XMAS";
+
+fn read_file(path: &str) -> Vec<String> {
+    read_to_string(path)
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect()
+}
+
+fn reverse_string(string: &String) -> String {
+    let mut output: String = String::new();
+    for character in string.chars().rev() {
+        output.push(character);
+    }
+    output
+}
+
+fn string_from_indices<const N: usize>(
+    data: &Vec<String>,
+    i_indices: [usize; N],
+    j_indices: [usize; N],
+) -> Option<String> {
+    let mut output = String::new();
+    for i in 0..N {
+        output.push(data.get(i_indices[i])?.chars().nth(j_indices[i])?);
+    }
+    Some(output)
+}
+
+fn check_horizontals(data: &Vec<String>) -> usize {
+    let mut total_count: usize = 0;
+    let word: String = String::from(WORD);
+    for string in data {
+        for i in 0..string.len() - word.len() {
+            let puzzle_word: String = String::from(&string[i..i + word.len()]);
+            let reversed_puzzle_word: String = reverse_string(&puzzle_word);
+            println!("{} {} {}", word, puzzle_word, reversed_puzzle_word);
+            if puzzle_word == word || reversed_puzzle_word == word {
+                total_count += 1;
+            }
+        }
+    }
+    total_count
+}
+
+fn check_verticles(data: &Vec<String>) -> usize {
+    let mut total_count: usize = 0;
+    let word: String = String::from(WORD);
+    for i in 0..data.len() - word.len() {
+        for j in 0..data[0].len() {
+            let i_indices: [usize; WORD.len()] = from_fn(|index| i + index);
+            let j_indices: [usize; WORD.len()] = [j; WORD.len()];
+            let puzzle_word = string_from_indices(data, i_indices, j_indices).unwrap();
+            let reversed_puzzle_word = reverse_string(&puzzle_word);
+            if puzzle_word == word || reversed_puzzle_word == word {
+                total_count += 1;
+            }
+        }
+    }
+    total_count
+}
+
+fn check_rising_diagonals(data: &Vec<String>) -> usize {
+    let mut total_count: usize = 0;
+    let word: String = String::from(WORD);
+    for i in word.len()..data.len() {
+        for j in 0..data[0].len() - word.len() {
+            let i_indices: [usize; WORD.len()] = from_fn(|index| i - index);
+            let j_indices: [usize; WORD.len()] = from_fn(|index| j + index);
+            let puzzle_word = string_from_indices(data, i_indices, j_indices).unwrap();
+            let reversed_puzzle_word = reverse_string(&puzzle_word);
+            if puzzle_word == word || reversed_puzzle_word == word {
+                total_count += 1;
+            }
+        }
+    }
+    total_count
+}
+
+fn check_falling_diagonals(data: &Vec<String>) -> usize {
+    let mut total_count: usize = 0;
+    let word: String = String::from(WORD);
+    for i in 0..data.len() - word.len() {
+        for j in 0..data[0].len() - word.len() {
+            let i_indices: [usize; WORD.len()] = from_fn(|index| i + index);
+            let j_indices: [usize; WORD.len()] = from_fn(|index| j + index);
+            let puzzle_word = string_from_indices(data, i_indices, j_indices).unwrap();
+            let reversed_puzzle_word = reverse_string(&puzzle_word);
+            if puzzle_word == word || reversed_puzzle_word == word {
+                total_count += 1;
+            }
+        }
+    }
+    total_count
+}
+fn part_1() {
+    let data: Vec<String> = read_file("inputs\\4test");
+    let mut total_count = 0;
+    total_count += check_horizontals(&data);
+    println!("{}", total_count);
+    total_count += check_verticles(&data);
+    println!("{}", total_count);
+    total_count += check_falling_diagonals(&data);
+    println!("{}", total_count);
+    total_count += check_rising_diagonals(&data);
+    println!("{}", total_count);
+}
 
 fn part_2() {}
 
